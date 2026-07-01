f791f3a08fbd01ba50ccbac008cf66e459032528
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-01 13:24:00 -0500

    claude autosave 2026-07-01T18:24:00Z

--- src/des/general/soccer.rs
+++ src/des/general/soccer.rs
@@ -51980,6 +51980,7 @@ fn tracking_frame_to_world_snapshot(
         winger_pinch_head: None,
         separation_floor_head: None,
         pass_lane_yield_head: None,
+        head_scan_head: None,
         long_pass_run_head: None,
         give_and_go_head: None,
         attack_spacing_head: None,
