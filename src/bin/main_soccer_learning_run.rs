0ce52af8bd48326fcee1aebfe09d6915f129b203
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-01 12:39:53 -0500

    claude autosave 2026-07-01T17:39:53Z

--- src/bin/main_soccer_learning_run.rs
+++ src/bin/main_soccer_learning_run.rs
@@ -13,8 +13,9 @@ use serde::Serialize;
 use soccer_engine::des::general::soccer::{
     soccer_moment_records_from_jsonl, soccer_moment_records_to_learning_dataset,
     train_soccer_pass_completion_head, AttackSpacingHead, BackFourLineHead, GiveAndGoHead,
-    LongPassRunHead, LooseBallCommitHead, MatchConfig, MatchSummary, ReceiveApproachHead,
-    ShotTriggerHead, SoccerConfigMomentInsert, SoccerMarlAlgorithm, SoccerMatch,
+    LaneAffinityHead, LongPassRunHead, LooseBallCommitHead, MatchConfig, MatchSummary,
+    ReceiveApproachHead, ShotTriggerHead, SoccerConfigMomentInsert, SoccerMarlAlgorithm,
+    SoccerMatch,
     SoccerMomentWindow, SoccerNeuralLearningBackend, SoccerNeuralLearningConfig,
     SoccerNeuralNetworkSnapshot, SoccerPassCompletionHead, SoccerPassLearningMetrics,
     SoccerPassOutcomeSample, SoccerQEntry, SoccerQPolicy, SoccerQPolicyOptions, SoccerQTargetEntry,
