4df28f22623c3ea7b598a836a1ac934fc94c49fc
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-01 13:08:55 -0500

    claude autosave 2026-07-01T18:08:55Z

--- src/des/general/soccer.rs
+++ src/des/general/soccer.rs
@@ -100,6 +100,8 @@ mod goal_side_recovery_decision;
 pub use goal_side_recovery_decision::*;
 mod winger_pinch_decision;
 pub use winger_pinch_decision::*;
+mod separation_floor_decision;
+pub use separation_floor_decision::*;
 mod policy_select;
 pub use policy_select::*;
 mod pass_lane_yield;
