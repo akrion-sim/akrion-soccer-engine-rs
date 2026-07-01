008cdf31ece7718ba9f853272620499b8cdfd308
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-01 17:47:14 -0500

    claude autosave 2026-07-01T22:47:14Z

--- src/des/general/soccer.rs
+++ src/des/general/soccer.rs
@@ -52659,6 +52659,7 @@ fn tracking_frame_to_world_snapshot(
         ranked_aerial_pass_cache: std::cell::RefCell::new(std::collections::HashMap::new()),
         support_decision_feature_cache: std::cell::RefCell::new(std::collections::HashMap::new()),
         line_depth_head: None,
+        defender_line_head: None,
         back_four_line_latch_centre_depth: [None, None],
         pass_completion_head: None,
         loose_ball_commit_head: None,
