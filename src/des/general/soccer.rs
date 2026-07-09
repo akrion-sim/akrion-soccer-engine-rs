9fa29c38501d2d7a245a1cbb0592e1df7b50f1f5
Alexander Mills <alexander.d.mills@gmail.com>
2026-07-09 10:51:14 -0500

    claude autosave 2026-07-09T15:51:14Z

--- src/des/general/soccer.rs
+++ src/des/general/soccer.rs
@@ -42832,6 +42832,7 @@ fn soccer_policy_head_snapshot(head: &SoccerPolicyHead) -> SoccerPolicyHeadSnaps
             .iter()
             .map(soccer_policy_role_head_snapshot)
             .collect(),
+        forward_select_logit_weight: head.forward_select_logit_weight,
     }
 }
 
